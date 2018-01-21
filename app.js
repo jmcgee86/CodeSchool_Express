133a41d334533eb1a5763396b96fadee75419db1
Jim McGee <jmcgee86@gmail.com>
2018-01-21 15:22:29 +0000

    level one complete

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ app.get('/', function (request, response) {
   response.send('Hello World');
 });
 
+app.get('/name', function (request, response){
+  var myName = 'Jim'
+  response.send(myName);
+});
+
+app.get('/redirect', function (request, response){
+  response.redirect(301,'/surprise');
+});
+
+app.get('/surprise', function(request, response){
+  response.send("You have been redirected");
+});
+
+app.get('/date', function(request, response){
+  response.send("Today is " + new Date());
+})
+
 app.listen(process.env.PORT, function(){
     console.log("Express is Running");
 });
