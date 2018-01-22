8a8467bfb8418d22e594c4cee95f9e04d0197be3
Jim McGee <jmcgee86@gmail.com>
2018-01-22 23:37:55 +0000

    add ajax call to client.js

--- public/client.js
+++ public/client.js
@@ -6,9 +6,27 @@ $(function(){
     
     function appendToList(cities){
         var list = [];
+        var content, city
         for (var i in cities){
-            list.push($('<option>', {text: cities[i]} ));
+            city = cities[i];
+            content = '<a href ="/cities/'+city+'">' + city + '</a>';
+            list.push($('<option>', {html: content} ));
         }
         $('#cityList').append(list);
     }
+    
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        var form = $(this);
+        var cityInfo = form.serialize();
+        
+        $.ajax({
+            type: 'POST', url: '/cities', data: cityInfo
+        }).done(function(cityName){
+                appendToList([cityName]);
+                form.trigger('reset');
+            });
+    });
 });
+
+
